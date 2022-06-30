9a0fedfd9e6f2c4e16d874e81b8908944029960c
INT\myerolemou <m.yerolemou@jcc.com.cy>
2022-06-30 13:08:14 +0300

    Initial Commit

---
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,49 @@
+﻿using BuberDinner.Contracts.Authentication;
+using BuberDinner.Application.Services.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using IAuthenticationService = BuberDinner.Application.Services.Authentication.IAuthenticationService;
+
+namespace BuberDinner.Api.Controllers;
+
+[ApiController]
+[Route("auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+    private readonly ILogger<AuthenticationController> _logger;
+
+    public AuthenticationController(IAuthenticationService authenticationService, ILogger<AuthenticationController> logger)
+    {
+        _authenticationService = authenticationService;
+        _logger = logger;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterRequest request)
+    {
+        var authResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName,
+            authResult.Email, authResult.Token);
+
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest request)
+    {
+        var authResult = _authenticationService.Login(
+            request.Email,
+            request.Password);
+
+        var response = new AuthenticationResponse(authResult.Id, authResult.FirstName, authResult.LastName,
+            authResult.Email, authResult.Token);
+
+        return Ok(response);
+    }
+}
